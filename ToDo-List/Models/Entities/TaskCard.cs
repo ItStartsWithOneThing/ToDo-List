372a4d64f483cecff615342ee884085712e08e9f
Kostiantyn Turkin <like69.kt@gmail.com>
2024-01-30 16:36:48 +0200

    Add project files.

---
+++ ToDo-List/Models/Entities/TaskCard.cs
@@ -0,0 +1,14 @@
+﻿namespace ToDo_List.Models.Entities
+{
+    public class TaskCard
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string? Text { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public bool Completed { get; set; }
+        public string BackgroundColor { get; set; } = "white";
+        public string TextColor { get; set; } = "black";
+        public string TitleColor { get; set; } = "black";
+    }
+}

bcfc981de31fccc37a3dc6d06240cae15168607c
Kostiantyn Turkin <like69.kt@gmail.com>
2024-03-05 15:10:51 +0200

    added exception filter and global exception resolver

---
+++ ToDo-List/Controllers/ExceptionResolvers/GlobalExceptionResolver.cs
@@ -0,0 +1,36 @@
+﻿
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+using ToDo_List.Models.API.Responses;
+
+namespace ToDo_List.Controllers.ExceptionResolvers
+{
+    public class GlobalExceptionResolver : IExceptionResolver
+    {
+        private readonly ILogger _logger;
+        private readonly IWebHostEnvironment _env;
+
+        public GlobalExceptionResolver(
+            ILogger<GlobalExceptionResolver> logger,
+            IWebHostEnvironment webHostEnvironment)
+        {
+            _logger = logger;
+            _env = webHostEnvironment;
+        }
+        public void OnException(ExceptionContext context)
+        {
+            var id = Guid.NewGuid();
+            var errorResponse = new ErrorResponse
+            {
+                Code = 500,
+                ErrorMessage = _env.IsProduction()
+                    ? "Internal server error"
+                    : context.Exception.ToString()
+            };
+
+            _logger.LogCritical(context.Exception, $"ErrorId : {id}");
+            context.HttpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+            context.Result = new ObjectResult(errorResponse);
+        }
+    }
+}
---
+++ ToDo-List/Controllers/ExceptionResolvers/IExceptionResolver.cs
@@ -0,0 +1,10 @@
+﻿
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace ToDo_List.Controllers.ExceptionResolvers
+{
+    public interface IExceptionResolver
+    {
+        public void OnException(ExceptionContext context);
+    }
+}
